2ef7eb79e77c12f447e969d9c73fe39c031cfdff
kitkit922 <115593395+kitkit922@users.noreply.github.com>
2023-02-19 12:48:09 -0500

    Completed v1.0 GUI

---
+++ img/Controller.java
@@ -0,0 +1,22 @@
+package assessment.assignment03.v1_0;
+
+import javax.swing.*;
+
+
+/**
+ * Assignment: #03
+ * ID: n01555831
+ * Name: Tsz Kit Cheung (Kevin Cheung)
+ *
+ * @version v1.0
+ * Controller
+ *
+ */
+
+public class Controller {
+    public static void main(String args[]) {
+        JFrame frame = new mainframe();
+        frame.setDefaultCloseOperation(3);
+        frame.setVisible(true);
+    }
+}
